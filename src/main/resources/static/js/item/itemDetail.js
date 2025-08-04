987837dbd3e97fd1105ba422b1f4acb4802c49a1
leehoju7 <hoju.lee@mail.utoronto.ca>
2025-08-04 13:24:49 +0900

    edited item detail feature

--- src/main/resources/static/js/item/itemDetail.js
+++ src/main/resources/static/js/item/itemDetail.js
@@ -7,17 +7,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
     btn.addEventListener('click', () => {
         if (!wrapper.classList.contains('expanded')) {
-            // 펼치기
             const fullH = content.scrollHeight;
             content.style.height = fullH + 'px';
             wrapper.classList.add('expanded');
             btn.textContent = 'Read less';
-            // 애니메이션 끝나면 auto 로 풀어 줘서 내부 크기에 따라 늘어나게
             setTimeout(() => {
                 content.style.height = 'auto';
-            }, 600); // CSS transition-duration(0.6s)과 맞추세요
+            }, 600);
         } else {
-            // 접기
             content.style.height = collapsedH + 'px';
             wrapper.classList.remove('expanded');
             btn.textContent = 'Read more';
