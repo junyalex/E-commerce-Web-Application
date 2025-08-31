80a845f2c9d6b4c85f8d9d280fd09ca7d0868bde
leehoju7 <hoju.lee@mail.utoronto.ca>
2025-08-31 10:47:55 -0400

    edited main page

--- src/main/java/com/example/shop/config/DataInitializer.java
+++ src/main/java/com/example/shop/config/DataInitializer.java
@@ -1,47 +1,47 @@
-//package com.example.shop.config;
-//
-//import com.example.shop.constant.ItemSellStatus;
-//import com.example.shop.constant.ItemType;
-//import com.example.shop.constant.Role;
-//import com.example.shop.entity.Item;
-//import com.example.shop.entity.Member;
-//import com.example.shop.repository.ItemRepository;
-//import com.example.shop.repository.MemberRepository;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.stereotype.Component;
-//
-//import java.math.BigDecimal;
-//
-//@Component
-//@RequiredArgsConstructor
-//public class DataInitializer implements CommandLineRunner {
-//
-//    private final MemberRepository memberRepository;
-//    private final PasswordEncoder passwordEncoder;
-//    private final ItemRepository itemRepository;
-//
-//    @Override
-//    public void run(String... args) throws Exception {
-//        if (memberRepository.findByEmail("user@test.com") == null) {
-//            Member user = new Member();
-//            user.setName("user");
-//            user.setEmail("user@test.com");
-//            user.setAddress("Seoul");
-//            user.setPassword(passwordEncoder.encode("1111"));
-//            user.setRole(Role.USER);
-//            memberRepository.save(user);
-//        }
-//
-//        if (memberRepository.findByEmail("admin@test.com") == null) {
-//            Member user = new Member();
-//            user.setName("admin");
-//            user.setEmail("admin@test.com");
-//            user.setAddress("Seoul");
-//            user.setPassword(passwordEncoder.encode("1234"));
-//            user.setRole(Role.ADMIN);
-//            memberRepository.save(user);
-//        }
-//    }
-//}
+package com.example.shop.config;
+
+import com.example.shop.constant.ItemSellStatus;
+import com.example.shop.constant.ItemType;
+import com.example.shop.constant.Role;
+import com.example.shop.entity.Item;
+import com.example.shop.entity.Member;
+import com.example.shop.repository.ItemRepository;
+import com.example.shop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+@RequiredArgsConstructor
+public class DataInitializer implements CommandLineRunner {
+
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final ItemRepository itemRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (memberRepository.findByEmail("user@test.com") == null) {
+            Member user = new Member();
+            user.setName("user");
+            user.setEmail("user@test.com");
+            user.setAddress("Seoul");
+            user.setPassword(passwordEncoder.encode("1111"));
+            user.setRole(Role.USER);
+            memberRepository.save(user);
+        }
+
+        if (memberRepository.findByEmail("admin@test.com") == null) {
+            Member user = new Member();
+            user.setName("admin");
+            user.setEmail("admin@test.com");
+            user.setAddress("Seoul");
+            user.setPassword(passwordEncoder.encode("1234"));
+            user.setRole(Role.ADMIN);
+            memberRepository.save(user);
+        }
+    }
+}
